d3afbffb95eabed3e12013b0de3afb6cc94d7201
DavWer <david.wernryd@telia.com>
2025-02-12 09:07:48 +0100

    fixade sorting till hot products

--- areas/products/services.py
+++ areas/products/services.py
@@ -1,5 +1,6 @@
 from models import Category, Product
 
+
 def getTrendingCategories():
     return Category.query.order_by(Category.CategoryID.desc()).paginate(page=1,per_page=4,error_out=False).items
 
@@ -9,5 +10,13 @@ def getCategory(id):
 def getProduct(id):
     return Product.query.filter(Product.ProductID ==id).first()
 
-def getTrendingProducts():
-    return Product.query.order_by(Product.ProductID.desc()).paginate(page=1,per_page=8,error_out=False).items
+def getTrendingProducts(sort_by="latest"):
+
+    if sort_by == "price":
+        return Product.query.order_by(Product.UnitPrice.asc()).paginate(page=1,per_page=8,error_out=False).items
+    elif sort_by == "name":
+        return Product.query.order_by(Product.ProductName.asc()).paginate(page=1,per_page=8,error_out=False).items
+    elif sort_by == "asc":
+        return Product.query.order_by(Product.ProductID.asc()).paginate(page=1,per_page=8,error_out=False).items
+    else:
+        return Product.query.order_by(Product.ProductID.desc()).paginate(page=1,per_page=8,error_out=False).items
